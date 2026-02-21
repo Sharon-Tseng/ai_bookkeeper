83da19498984357539b65bf5db066a3d984211a8
Sharon Tseng <sharontseng8912@gmail.com>
2026-02-21 16:33:53 +0800

    include secrets.dart for vercel build

---
+++ bookkeeper_flutter/lib/services/secrets.dart
@@ -0,0 +1,15 @@
+const String googleServiceAccountJson = r'''
+{
+  "type": "service_account",
+  "project_id": "utopian-nimbus-482915-d0",
+  "private_key_id": "6b7101249209ef919b904333336521e725f388f2",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDcgQng+aEXOyuq\n9to9KLURnxLX3zWW9eWzh8JCW3f83O8r+jXcZfS5u/ZwTkyAWuLvhlz3qKwpiSmh\niDh7DnebKFNis8HgFqjGJKE2Su/Gm7r168znwyN9MuskWnddKSIVgXR9TTDNIoxD\nGAgRTIiiZSE9n5Uxzpj1bJckDclE9yn+xvdobTNTUwstguh11tt1uOzjGqgefSkZ\nIQFB44EH2gmi/lKNvUJ8kuekZqrWzk86T0ZQaVspapVsUmoKWWbYX1FBaLoyHlr6\n3zIlPz9QRklhVrkKMP9uRymwg57Op9mFld4Zc3MSwUa/2aJY/ABFp7aVwysgBC29\n/uLkVuCtAgMBAAECggEAMt3Ql9W2L9v+P5bo8tVWac16pRJQdDXe6oqdiT29APe9\nc7VpYeGiMgt2duNVGgH9WlhyT05Zgs5prGH2d4q3lDMaA0mo5ocLoYPXLaVdSUFp\nAGyILQmywOGzZUQ6R2MjAfeGVhZttNCsrxiW2djAJ0gxghvzaJU6a1lEx3QnszDs\ngqFSLzHoHIxD03016Xmzgt3fPkXJ+HuADU5wdA40sSrWXTY3+OHE4m+SuPXWY7Wc\n4B0S1KERwGJ4ywSui9rkZIjsSxvV2atISlzzRp8Gbl3fWCq/gqcvsaBudHP7jOVY\nKYv6Yh587jCTTEcVQlRAzCFKdAaCrZljuvFZTd0woQKBgQD43cqyVzytseRzxFyV\nyzQlJbpvtJTAg4xcFBbl7bE2BXQZHC1VM7eHn1bcEDk+XrvGitE5bbeDKhKMRZJQ\nZzT525aFLBVvtrVdW0eiK1vOx336YbLKpUzyNpUm2tjVWLhpWT4/Z1D96fn79AKw\nngHHtg2wT1NNIEJDrdC73Z6yTQKBgQDi0x7+keA7fUx2+rpTJV+x8mGJqcvJvS4U\n2WlKzi0XH8BZeBqxQr5fjYSilHFwOAxK6NINcwjoXk0mIOEVurSySabCm9CfX0to\nvX8GHQuKayLDFVSHT5Km9ytbaOkfRYiaHULJ/x6GKmiAHfjGsA8+3cWtRN32l7bz\n9ZGGXFRX4QKBgHWBQ5OxXRUmSZffv1d6+sQPbFgSC6pacouHcMhf0qyPJqI0i7MP\nGDkRIxIFFwI7+K1mHiPfiiaEVv2kW9Fa6ciPxiJbDkQRINFTqB8KGe8ognMWbjUA\nl7RF0WbnLjqOCYS0fo+xWKyLLV2peRurf2usqgn1an+gG6mZCFWr7qKBAoGAMojI\nyfykEXzr8UIRCv8zn8iZdHsw3ORaBOkfLTxcDV/UkdpRUBbHpTA5UaNy1oaWLoZK\nbhan8GU6GPmQTDAJnAMOtSLTyv4HJfG6ABmufYs69m2lJHRU/v/oV/g6n+307ikA\nvVIqcce2+nwy9ck5zE7KgLzsGslYSGf4oQ+m9uECgYEAu62VIGexqfVOm3BkgY2R\nNyMNSboNg2raJ2U1u7r8K4ai/ajPjiUUIVOLfDTXIIhTP98k6WB1WiPKjqgQK1Ml\nTed7ooG0wiFEZ1pIF5v8ZjCfhuwDjstzkWOOFzkVAtobi7pU0ohQNFIQPhQy/0Ja\ne1Q+6gr3J2WDalPFxFOuamk=\n-----END PRIVATE KEY-----\n",
+  "client_email": "bookkeeper-agent@utopian-nimbus-482915-d0.iam.gserviceaccount.com",
+  "client_id": "105793155482374595226",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/bookkeeper-agent%40utopian-nimbus-482915-d0.iam.gserviceaccount.com",
+  "universe_domain": "googleapis.com"
+}
+''';
